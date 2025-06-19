f5b2aaf59d2a15dd3210fc02ab3f19a8614d532c
zimo493 <2081182432@qq.com>
2025-06-19 17:03:06 +0800

    feat(vitepress): 添加图片查看器功能
    - 新增 ImgViewer 组件，实现图片放大全屏查看功能
    - 在主题初始化时绑定图片查看器
    - 在路由切换时销毁和重新绑定图片查看器
    - 优化图片 alt 属性和标题显示

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,7 +1,9 @@
-import { h } from "vue";
+import { h, onMounted, onUnmounted } from "vue";
 import DefaultTheme from "vitepress/theme";
-import type { Theme as ThemeConfig } from "vitepress";
+import { type Theme as ThemeConfig, inBrowser } from "vitepress";
 import { NolebaseGitChangelogPlugin } from "@nolebase/vitepress-plugin-git-changelog/client";
+
+import { bindFancybox, destroyFancybox } from "./components/ImgViewer"; // 图片查看器
 
 import GiscusComment from "./components/GiscusComment"; // 评论模块
 
@@ -12,7 +14,17 @@ export const Theme: ThemeConfig = {
   Layout: h(DefaultTheme.Layout, null, {
     "doc-after": () => [h(GiscusComment)],
   }),
-  enhanceApp({ app }) {
+  enhanceApp({ app, router }) {
+    if (inBrowser) {
+      router.onBeforeRouteChange = () => {
+        destroyFancybox(); // 销毁图片查看器
+        console.log(3);
+      };
+      router.onAfterRouteChange = () => {
+        bindFancybox(); // 绑定图片查看器
+        console.log(4);
+      };
+    }
     app.use(NolebaseGitChangelogPlugin, {
       displayAuthorsInsideCommitLine: true,
     });
